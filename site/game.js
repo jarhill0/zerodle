9cca8e176aa8aa1224ce688d3b181c07c4d4824a
jarhill0 <git@reeshill.net>
2022-04-11 20:51:15 -0400

    Add rudimentary share buttons

--- site/game.js
+++ site/game.js
@@ -1,4 +1,42 @@
+window.onload = function() {
+    if (navigator.share) {
+        document.getElementById("game-share-button").classList.remove("hidden");
+    }
+};
+
+
 function gameNumber() {
     // TODO
     return 7;
+}
+
+function gameText() {
+    // TODO
+    return "my game game!";
+}
+
+function shareGame() {
+    const text = gameText();
+    navigator.share({
+        text: text
+    }); // not doing anything with returned promise
+}
+
+function copyGame() {
+    const text = gameText();
+    navigator.clipboard.writeText(text).then(
+        textCopied,
+        function() {
+            // clipboard write failed :(
+        });
+}
+
+let timeout = null;
+function textCopied() {
+    button = document.getElementById("game-copy-button");
+    button.innerText = "Game copied!";
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+        button.innerText = "Copy";
+    }, 3000);
 }
